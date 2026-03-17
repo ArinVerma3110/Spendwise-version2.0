db0da27c97dd7326bae0974863627430a1be003e
Arin Verma <149854813+ArinVerma3110@users.noreply.github.com>
2026-03-17 18:42:18 +0530

    Update config.py

--- Spendwise/config.py
+++ Spendwise/config.py
@@ -1,18 +1,11 @@
-# MYSQL_HOST = 'localhost'
-# MYSQL_USER = 'root'
-# MYSQL_PASSWORD = 'av31' #  <------ Write your paasword here
-# MYSQL_DB = 'spendwise' #   <------ Write your schema name here
-
-
-# config.py
 
 # --- MySQL Configuration ---
-MYSQL_HOST = 'localhost'         # usually 'localhost' for local dev
-MYSQL_USER = 'root'              # your MySQL username
-MYSQL_PASSWORD = 'av31'  # <-- replace with your MySQL password
-MYSQL_DB = 'spendwise'           # database name used for this project
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'root'
+MYSQL_PASSWORD = 'av31'   # <------ Write your paasword here
+MYSQL_DB = 'spendwise'    # <------ Write your schema name here
 
 # --- Flask Secret Key ---
 # Required for user sessions, flash messages, and login security.
 # Use a long random string in production.
-SECRET_KEY = 'ilovepizza'
+SECRET_KEY = 'ilovepizza' # <----- Enter your secret key here
